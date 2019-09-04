0b300c2376170834822492b73d02c89e958453f3
Madhu9000 <podapati.madhunaidu@gmail.com>
2019-09-04 17:55:25 +0530

    register method implemented

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -31,6 +31,7 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(options =>options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors();
+            services.AddScoped<UserAuthRepository, UserAuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
