3b81f2f1d9c8647b27eba2d53d220afccddbc6d4
Prince Kumar <prince_kumar@persistent.co.in>
2019-09-18 17:38:30 +0530

    End of section 9

---
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { User } from 'src/app/_models/user';
+import { UserService } from 'src/app/_services/user.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-member-list',
+  templateUrl: './member-list.component.html',
+  styleUrls: ['./member-list.component.css']
+})
+export class MemberListComponent implements OnInit {
+  users: User[];
+
+  constructor(private userService: UserService, private alertify: AlertifyService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      // tslint:disable-next-line: no-string-literal
+      this.users = data['users'];
+    });
+  }
+}
